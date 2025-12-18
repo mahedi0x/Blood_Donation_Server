029ecd6418e35040189bf7b1e33433eb147d944f
Mahedi0x <mahedih.cse@gmail.com>
2025-12-19 00:32:30 +0600

    donation req not working

--- index.js
+++ index.js
@@ -231,12 +231,12 @@ async function run() {
       }
     );
 
-    // app.delete("/donation-requests/:id", verifyFBToken, async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await donationRequestsCollection.deleteOne(query);
-    //   res.send(result);
-    // });
+    app.delete("/donation-requests/:id", verifyFBToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await donationRequestsCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
